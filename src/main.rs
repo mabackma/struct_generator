49454af0d19ec5736acc7d39eec7e32b1ff6bb07
mabackma <marc.backman@edu.lapinamk.fi>
2024-12-10 10:29:40 +0200

    creates structs from all .xsd files

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,44 @@
-use struct_generator::create_structs::{XMLStruct, create_structs};
+use struct_generator::create_structs::{create_structs, XMLStruct};
 use struct_generator::string_utils::to_snake_case;
 
 use std::collections::HashMap;
+use std::fs;
 use std::fs::File;
 use std::io::{self, Read, Write};
 use quick_xml::Reader;
 
 fn main() {
-    let xsd_content = &read_xsd_file("schema.xsd").unwrap();
-    let mut reader = Reader::from_str(xsd_content);
+    let folder_path = "./MetsatietostandardiskeematV33.03/V33"; // Path to your folder containing XSD files
     let mut structs: HashMap<String, XMLStruct> = HashMap::new(); // Finalized structs
     let mut element_definitions: HashMap<String, String> = HashMap::new(); // Definitions for elements
 
-    create_structs(&mut reader, &mut structs, &mut element_definitions);
+    // Read and process all XSD files in the folder
+    let xsd_files = process_folder(folder_path).unwrap();
+
+    for xsd_content in xsd_files.iter() {
+        let mut reader = Reader::from_str(xsd_content);
+        create_structs(&mut reader, &mut structs, &mut element_definitions);
+    }
 
     structs_to_file(&structs, "structs.rs").unwrap();
+    element_definitions_to_file(&element_definitions, "element_definitions.rs").unwrap();
+}
+
+fn process_folder(folder_path: &str) -> Result<Vec<String>, std::io::Error> {
+    let mut xsd_contents = Vec::new();
+
+    for entry in fs::read_dir(folder_path)? {
+        let entry = entry?;
+        let path = entry.path();
+
+        // Check if the entry is a file and has a `.xsd` extension
+        if path.is_file() && path.extension().and_then(|ext| ext.to_str()) == Some("xsd") {
+            let content = read_xsd_file(path.to_str().unwrap())?;
+            xsd_contents.push(content);
+        }
+    }
+
+    Ok(xsd_contents)
 }
 
 // Reads an XML file and returns its contents as a string
@@ -36,7 +60,7 @@ fn structs_to_file(structs: &HashMap<String, XMLStruct>, file_name: &str) -> io:
 
     // Build the structs as a string
     for (name, xml_struct) in structs.iter() {
-        structs_string.push_str(&format!("\n#[derive(Debug, Serialize, Deserialize)]"));
+        structs_string.push_str(&format!("#[derive(Debug, Serialize, Deserialize)]"));
         structs_string.push_str(&format!("\npub struct {} {{\n", name));
 
         for field in xml_struct.fields.iter() {
@@ -51,12 +75,27 @@ fn structs_to_file(structs: &HashMap<String, XMLStruct>, file_name: &str) -> io:
             structs_string.push_str(&format!("    pub {}: {},\n", to_snake_case(&field.name), field.field_type));
         }
 
-        structs_string.push_str("}\n");
+        structs_string.push_str("}\n\n");
     }
 
     // Write the string to the file
     let mut file = File::create(file_name)?;
     file.write_all(structs_string.as_bytes())?;
+
+    Ok(())
+}
+
+fn element_definitions_to_file(element_definitions: &HashMap<String, String>, file_name: &str) -> io::Result<()> {
+    let mut element_definitions_string = String::new();
+
+    // Build the element definitions as a string
+    for (name, typ) in element_definitions.iter() {
+        element_definitions_string.push_str(&format!("pub const {}: &str = \"{}\";\n", to_snake_case(name), typ));
+    }
+
+    // Write the string to the file
+    let mut file = File::create(file_name)?;
+    file.write_all(element_definitions_string.as_bytes())?;
 
     Ok(())
 }
