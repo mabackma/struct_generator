4b056db66e602dcca3f2b1da44695de4af55bc4c
mabackma <marc.backman@edu.lapinamk.fi>
2024-12-10 03:00:08 +0200

    writing structs to file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{self, Read};
+use std::io::{self, Read, Write};
 use quick_xml::events::BytesStart;
 use quick_xml::Reader;
 use quick_xml::events::Event::{Start, End, Empty, Eof};
@@ -41,23 +41,9 @@ fn main() {
     let mut reader = Reader::from_str(xsd_content);
     let mut structs: HashMap<String, XMLStruct> = HashMap::new(); // Finalized structs
 
-    parse_nested_elements(&mut reader, &mut structs);
+    create_structs(&mut reader, &mut structs);
 
-/*     for (name, xml_struct) in structs.iter() {
-        println!("\npub struct {} {{", name);
-
-        for field in xml_struct.fields.iter() {
-            println!("    {}: {},", field.name, field.field_type);
-        }
-
-        println!("}}");
-    } */
-
-    for s in structs {
-        //if s.1.name == ""
-        println!("{:#?}", s);
-        println!();
-    }
+    structs_to_file(&structs, "structs.rs").unwrap();
 }
 
 // Reads an XML file and returns its contents as a string
@@ -74,65 +60,11 @@ fn read_xsd_file(file_name: &str) -> io::Result<String> {
     Ok(xsd_string)
 }
 
-// Parse the element
-fn parse_element(e: &BytesStart<'_>, xml_struct: &mut XMLStruct) {
-    let mut name = element_references(e);
-    let mut e_type = None;
-
-    // If the element has no reference, get the name and type
-    if name.is_none() {
-        name = element_names(e);
-        e_type = element_types(e);
-    } else {
-        e_type = search_reference_type(name.as_ref().unwrap());
-    }
-
-    if is_element_vec(e) {
-        if is_element_optional(e) {
-            //print!("{}: Option<Vec<{}>>,", name.clone().unwrap(), e_type.clone().unwrap_or(name.clone().unwrap()));
-
-            xml_struct.fields.push(XMLField {
-                name: name.clone().unwrap(),
-                field_type: format!("Option<Vec<{}>>", e_type.clone().unwrap_or(name.clone().unwrap())),
-            });
-        } else {
-            //print!("{}: Vec<{}>,", name.clone().unwrap(), e_type.clone().unwrap_or(name.clone().unwrap()));
-
-            xml_struct.fields.push(XMLField {
-                name: name.clone().unwrap(),
-                field_type: format!("Vec<{}>", e_type.unwrap_or(name.clone().unwrap())),
-            });
-        }
-    } else {
-        if is_element_optional(e) {
-            //print!("{}: Option<{}>,", name.clone().unwrap(), e_type.clone().unwrap_or(name.clone().unwrap()));
-
-            xml_struct.fields.push(XMLField {
-                name: name.clone().unwrap(),
-                field_type: format!("Option<{}>", e_type.clone().unwrap_or(name.clone().unwrap())),
-            });
-        } else {
-            //print!("{}: {},", name.clone().unwrap(), e_type.clone().unwrap_or(name.clone().unwrap()));
-
-            xml_struct.fields.push(XMLField {
-                name: name.clone().unwrap(),
-                field_type: e_type.unwrap_or(name.clone().unwrap()),
-            });
-        }
-    }
-
-    //println!();
-}
-
 // Retrieve the element reference
 fn element_references(e: &BytesStart<'_>) -> Option<String> {
     let e_ref = e.attributes().filter_map(|a| a.ok())
         .find(|a| a.key == QName(b"ref")) 
         .and_then(|a| String::from_utf8(a.value.to_vec()).ok()); // Extract the ref attribute value as a string
-
-    if let Some(_) = e_ref {
-        //print!("ref: ");
-    }
 
     e_ref
 }
@@ -142,10 +74,6 @@ fn element_names(e: &BytesStart<'_>) -> Option<String> {
     let e_name = e.attributes().filter_map(|a| a.ok())
         .find(|a| a.key == QName(b"name")) 
         .and_then(|a| String::from_utf8(a.value.to_vec()).ok()); // Extract the name attribute value as a string
-
-    if let Some(_) = e_name {
-        //print!("name: ");
-    }
 
     e_name
 }
@@ -196,7 +124,7 @@ fn is_element_optional(e: &BytesStart<'_>) -> bool {
 }
 
 // Parse nested elements
-fn parse_nested_elements(
+fn create_structs(
     reader: &mut Reader<&[u8]>,
     structs: &mut HashMap<String, XMLStruct>,
 ) {
@@ -219,9 +147,7 @@ fn parse_nested_elements(
                             fields: Vec::new(),
                         };
 
-                        println!("Current struct: {}", current_name);
                         stack.push(new_struct.clone());
-                        println!("Stack length: {}", stack.len());
                     }
                 }
             }
@@ -235,15 +161,33 @@ fn parse_nested_elements(
                             name = element_references(e);
                         }
 
+                        name = Some(remove_prefix(&name.unwrap()));
+
                         if let Some(n) = name {
                             let mut field_type = n.clone();
 
                             if let Some(typ) = element_types(e) {
                                 field_type = typ;
                             } 
+                            
+                            field_type = remove_prefix(&field_type);
+
+                            // Check if the element is a vector or optional
+                            if is_element_vec(e) {
+                                if is_element_optional(e) {
+                                    field_type = format!("Option<Vec<{}>>", field_type);
+                                } else {
+                                    field_type = format!("Vec<{}>", field_type);
+                                }
+                            } else {
+                                if is_element_optional(e) {
+                                    field_type = format!("Option<{}>", field_type);
+                                }
+                            }
 
                             // Check if the field already exists
                             if !parent_struct.fields.iter().any(|field| field.name == n) {
+                                // Add the field to the parent struct
                                 parent_struct.fields.push(XMLField {
                                     name: n.clone(),
                                     field_type,
@@ -286,4 +230,57 @@ fn parse_nested_elements(
 fn search_reference_type(ref_name: &str) -> Option<String> {
     // Search for the reference type in the schema
     Some("String from ref".to_string())
+}
+
+fn structs_to_file(structs: &HashMap<String, XMLStruct>, file_name: &str) -> io::Result<()> {
+    let mut structs_string = String::new();
+
+    // Build the structs as a string
+    for (name, xml_struct) in structs.iter() {
+        structs_string.push_str(&format!("\npub struct {} {{\n", name));
+
+        for field in xml_struct.fields.iter() {
+            structs_string.push_str(&format!("    #[serde(rename = \"{}\", skip_serializing_if = \"Option::is_none\")]\n", field.name));
+            structs_string.push_str(&format!("    pub {}: {},\n", to_snake_case(&field.name), field.field_type));
+        }
+
+        structs_string.push_str("}\n");
+    }
+
+    // Write the string to the file
+    let mut file = File::create(file_name)?;
+    file.write_all(structs_string.as_bytes())?;
+
+    Ok(())
+}
+
+fn remove_prefix(name: &str) -> String {
+    let mut parts = name.split(':').collect::<Vec<&str>>();
+
+    if parts.len() > 1 {
+        parts.remove(0);
+        return parts.join("");
+    }
+
+    name.to_string()
+}
+
+fn to_snake_case(name: &str) -> String {
+    let mut snake_case = String::new();
+    let mut chars = name.chars();
+
+    if let Some(first_char) = chars.next() {
+        snake_case.push(first_char.to_ascii_lowercase());
+    }
+
+    for c in chars {
+        if c.is_uppercase() {
+            snake_case.push('_');
+            snake_case.push(c.to_ascii_lowercase());
+        } else {
+            snake_case.push(c);
+        }
+    }
+
+    snake_case
 }
